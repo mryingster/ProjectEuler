7c20e882310acf198a81c47c19742a5baeb58640
Michael Caldwell <michaelc@yingster.net>
2014-09-02 18:49:58 -0700

    Refining solution to problem 12. Optimized by: * Precalculating prime numbers * Performing prime factorization * Converting prime factorization to get number of divisors * Looking at every other triangle number (every other one has higher chances of factorability)

--- problem_012.c
+++ problem_012.c
@@ -2,41 +2,97 @@
 // Copyright (c) 2014 Michael Caldwell
 #include <stdio.h>
 
+int isPrime(int input, int *primes)
+{
+    int n=0;
+    if (input == 2)
+        return 1;
+    else
+        while (primes[n] != 0)
+        {
+            if (input % primes[n] == 0)
+                return 0;
+            n++;
+        }
+    return 1;
+}
+
+int findDivisors(int input, int *primes, int primesToFind)
+{
+    int i, count=1;
+    int primeCount[primesToFind];
+    for (i=0; i<primesToFind; i++)
+        primeCount[i] = 0;
+
+    // Prime factorization
+    while (input != 1)
+    {
+        for (i=0; i<primesToFind; i++)
+        {
+            if (input % primes[i] == 0)
+            {
+                input = input / primes[i];
+                primeCount[i]++;
+                //count++;
+            }
+        }
+    }
+
+    // Convert prime factorization to divisors
+    for (i=0; i<primesToFind; i++)
+    {
+        if (primeCount[i] > 0)
+        {
+            count=count*(primeCount[i]+1);
+        }
+    }
+
+    return count;
+}
+
 int main()
 {
-    int i, j, m, n, x, y;
-    int largest = 0, candidate = 0;
-    i=j=m=n=x=y=0;
+    int i=0, triangle=0, divisors=0;
+    int primeCount = 0;
+
+    int primesToFind = 1500;
+    int primes[primesToFind];
 
     printf("Project Euler - Problem 12:\n"
            "What is the value of the first triangle number to have over five hundred divisors?\n\n");
 
+    // Initialize primesToFind
+    for (i=0; i<primesToFind; i++)
+        primes[i] = 0;
 
-    for (i=1 ; i<1000000 ; i++)
+    // Pre-Calculate Primes
+    //printf("Calculating primes...\n");
+    i=1;
+    while (primeCount < primesToFind)
     {
-        m=0;
-        for (n=1 ; n<i; n++)
-	{
-            m=m+n;
-	}
-        j=m;
-        y=0;
-        for (x=1 ; x<j ; x++)
-	{
-            if (j % x)
-                printf("");
-            //printf("not divisible by %i\n", x);
-            else
-	    {
-                //printf("divisible by %i\n", x);
-                y=y+1;
-	    }
-	}
-        //printf("%i : %i : %i\n", i, j, y);
-        if (y>500)
+        i++;
+        if (isPrime(i, primes))
+        {
+            primes[primeCount]=i;
+            //printf("%i, %i\n", i, primeCount);
+            primeCount++;
+        }
+    }
+    //printf("Primes Calculated.\n");
+
+    // Calculate Triangles
+    i=0;
+    while(i<1000000)
+    {
+        i++;
+        triangle = triangle+i;
+        divisors = findDivisors(triangle, primes, primesToFind);
+        if (divisors > 500)
             break;
+        i++;
+        triangle = triangle+i;
     }
 
-    printf("Triangle Value: \033[1m%i\033[0m, Nth Triangle Number: %i, Divisors: %i\n", m, i, y);
+    printf("Triangle Value: \033[1m%i\033[0m, Divisors: %i\n", triangle, divisors);
     return 0;
 }
