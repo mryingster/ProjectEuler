f8465397a150bd3ea6922b62b55bf84772840a60
Michael Caldwell <michaelc@yingster.net>
2014-09-26 20:09:50 -0700

    Fixing some careless fromatting, and reducing code.

--- problem_003.c
+++ problem_003.c
@@ -4,29 +4,23 @@
 
 int main()
 {
+    unsigned long long bigNum = 600851475143;
+    int lastFactor = 0;
     int i;
-    unsigned long long bigNum;
-    int lastFactor;
-
-    bigNum=600851475143;
-    lastFactor=0;
 
     printf("Project Euler - Problem 3:\n"
            "What is the largest prime factor of the number 600851475143 ?\n\n");
 
-    for (i=2; i<10000; i++)
-    {
-        if ( bigNum % i )
-            printf("");
-        else
+    for ( i=2; i<10000; i++ )
+        if ( bigNum % i == 0 )
 	{
             //printf("Factor Found: %i\n", i);
-            bigNum=bigNum/i;
-            lastFactor=i;
-            i=2;
+            bigNum /= i;
+            lastFactor = i;
+            i = 2;
 	}
-    }
-    printf("Largest factor found: \033[1m%i\033[0m\n", lastFactor);
+
+    printf("Largest factor found: %d\n", lastFactor);
 
     return 0;
 }
