1045e7e1573f2de0fa28d25937350beb914f636a
Michael Caldwell <michaelc@yingster.net>
2014-09-27 22:29:15 -0700

    Switching prime check to use ceuler lib function, isPrimeOpt. Reducing code.

--- problem_007.c
+++ problem_007.c
@@ -1,44 +1,22 @@
-// -*- compile-command: "gcc -o problem_007 problem_007.c -Wall" -*-
+// -*- compile-command: "gcc -o problem_007 problem_007.c ceuler.c -Wall -lm" -*-
 // Copyright (c) 2014 Michael Caldwell
 #include <stdio.h>
-
-int isPrime(int input, int *primes)
-{
-    int n=0;
-
-    if (input == 2)
-        return 1;
-    else
-        while (primes[n] != 0)
-        {
-            if (input % primes[n] == 0)
-                return 0;
-            n++;
-        }
-    return 1;
-}
+#include <ceuler.h>
 
 int main()
 {
-    int primeCount=0, candidate=0;
+    int primeCount    = 0;
+    int candidate     = 1;
     int primes[10002] = {0};
 
     printf("Project Euler - Problem 7:\n"
            "What is the 10,001st prime number?\n\n");
 
-    for (candidate=2 ; candidate<10000000 ; candidate++)
-    {
-        if ( isPrime(candidate, primes) )
-        {
-            primes[primeCount] = candidate;
-            primeCount++;
-        }
-        if (primeCount == 10001)
-            break;
-    }
+    while ( primeCount < 10001 )
+        if ( isPrimeOpt(++candidate, primes) )
+            primes[primeCount++] = candidate;
 
-    printf("10,001st prime: \033[1m%i\033[0m\n", candidate);
-    //Answer: 104743
+    printf("10,001st prime: %d\n", candidate);
 
     return 0;
 }
