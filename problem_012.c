08054ef206ef7fda51890daea9977fa18e68ab2a
Michael Caldwell <michaelc@yingster.net>
2014-09-28 18:52:07 -0700

    Removing prime function and using isPrimeOpt function from ceuler library. Other small refinements.

--- problem_012.c
+++ problem_012.c
@@ -1,49 +1,35 @@
-// -*- compile-command: "gcc -o problem_012 problem_012.c -Wall" -*-
+// -*- compile-command: "gcc -o problem_012 problem_012.c ceuler.c -Wall -lm" -*-
 // Copyright (c) 2014 Michael Caldwell
 #include <stdio.h>
-
-int isPrime(int input, int *primes)
-{
-    int n=0;
-    if (input == 2)
-        return 1;
-    else
-        while (primes[n] != 0)
-        {
-            if (input % primes[n] == 0)
-                return 0;
-            n++;
-        }
-    return 1;
-}
+#include <ceuler.h>
 
 int findDivisors(int input, int *primes, int primesToFind)
 {
-    int i, count=1;
+    int index, count=1;
     int primeCount[primesToFind];
-    for (i=0; i<primesToFind; i++)
-        primeCount[i] = 0;
+    for (index=0; index<primesToFind; index++)
+        primeCount[index] = 0;
 
     // Prime factorization
     while (input != 1)
-        for (i=0; i<primesToFind; i++)
-            if (input % primes[i] == 0)
+        for (index=0; index<primesToFind; index++)
+            if (input % primes[index] == 0)
             {
-                input = input / primes[i];
-                primeCount[i]++;
+                input = input / primes[index];
+                primeCount[index]++;
             }
 
     // Convert prime factorization to divisors
-    for (i=0; i<primesToFind; i++)
-        if (primeCount[i] > 0)
-            count *= (primeCount[i]+1);
+    for (index=0; index<primesToFind; index++)
+        if (primeCount[index] > 0)
+            count *= (primeCount[index]+1);
 
     return count;
 }
 
 int main()
 {
-    int i=0, triangle=0, divisors=0;
+    int index=0, triangle=0, divisors=0;
     int primeCount = 0;
 
     int primesToFind = 1500;
@@ -53,30 +39,28 @@ int main()
            "What is the value of the first triangle number to have over five hundred divisors?\n\n");
 
     // Initialize primesToFind
-    for (i=0; i<primesToFind; i++)
-        primes[i] = 0;
+    for (index=0; index<primesToFind; index++)
+        primes[index] = 0;
 
     // Pre-Calculate Primes
-    //printf("Calculating primes...\n");
-    i=1;
+    index = 1;
     while (primeCount < primesToFind)
     {
-        i++;
-        if (isPrime(i, primes))
+        index++;
+        if (isPrimeOpt(index, primes))
         {
-            primes[primeCount]=i;
-            //printf("%i, %i\n", i, primeCount);
+            primes[primeCount]=index;
             primeCount++;
         }
     }
-    //printf("Primes Calculated.\n");
 
     // Calculate Triangles
-    i=0;
-    while(i<1000000)
+    index = 0;
+    while(index<1000000)
     {
-        i++;
-        triangle = triangle+i;
+        index++;
+        triangle += index;
+
         // Skip numbers not divisible by 3 or 5
         if ( (triangle % 3 != 0) || (triangle % 5 != 0) )
             continue;
@@ -86,6 +70,6 @@ int main()
             break;
     }
 
-    printf("Triangle Value: \033[1m%i\033[0m, Divisors: %i\n", triangle, divisors);
+    printf("Triangle Value: %d, Divisors: %d\n", triangle, divisors);
     return 0;
 }
