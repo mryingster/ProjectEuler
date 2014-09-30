55f34df19d47e80d08ba2193f628e1f804e6e56e
Michael Caldwell <michaelc@yingster.net>
2014-09-30 13:07:38 -0700

    Refining solution to problem 5. Removing escape characters from result text.

--- problem_005.c
+++ problem_005.c
@@ -1,31 +1,33 @@
 // -*- compile-command: "gcc -o problem_005 problem_005.c -Wall" -*-
 // Copyright (c) 2014 Michael Caldwell
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
-    int i, j, candidate;
+    int number = 1000;
+    int divisor;
+    bool candidate;
 
     printf("Project Euler - Problem 5:\n"
            "What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?\n\n");
 
-    for (i=1000 ; i<699000000 ; i=i+20)
+    while ( number += 20 )
     {
-        candidate=i;
-        for (j=1 ; j<21 ; j++)
-	{
-            if (i % j)
-	    {
-                candidate=0;
-                //printf("%i Not divisible by %i\n", i, j);
+        candidate = true;
+        for ( divisor=2 ; divisor<=20 ; divisor++ )
+        {
+            if ( number % divisor != 0 )
+            {
+                candidate = false;
                 break;
-	    }
-	}
-        if (candidate != 0)
+            }
+        }
+        if ( candidate == true )
             break;
     }
 
-  printf("Smallest number: \033[1m%i\033[0m\n", candidate);
+  printf("Smallest number: %d\n", number);
 
   return 0;
 }
