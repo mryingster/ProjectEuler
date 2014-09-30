908e37999d4752a04abe161b0065556977faa27b
Michael Caldwell <michaelc@yingster.net>
2014-09-30 12:54:12 -0700

    Cleaning up code. Switching to isPalindromic() function in ceuler lib.

--- problem_004.c
+++ problem_004.c
@@ -1,36 +1,34 @@
-// -*- compile-command: "gcc -o problem_004 problem_004.c -Wall" -*-
+// -*- compile-command: "gcc -o problem_004 problem_004.c ceuler.c -Wall -lm" -*-
 // Copyright (c) 2014 Michael Caldwell
 #include <stdio.h>
+#include <stdbool.h>
+#include <ceuler.h>
 
 int main()
 {
-    int i, j, m, n, palindrome, bigpalindrome;
-    char product[10];
+    int num1, num2;
+    int product;
+    int maxPalindrome = 0;
 
     printf("Project Euler - Problem 4:\n"
            "Find the largest palindrome made from the product of two 3-digit numbers.\n\n");
 
-    for (i=100 ; i<1000 ; i++)
+    for (num1=100 ; num1<1000 ; num1++)
     {
-        for (j=i ; j<1000 ; j++)
+        for (num2=num1 ; num2<1000 ; num2++)
         {
-            snprintf(product, sizeof(product), "%i", j*i);
-            //printf("%s\n", product);
-            palindrome = 1;
-
-            for (m=0 ; product[m]!='\0'; m++);
-            for (n=0 ; n<m ; n++)
-                if (product[n] != product[m-1-n])
-                    palindrome = 0;
-            if (palindrome == 1)
+            product = num1 * num2;
+            if (isPalindromic(product) == true)
             {
-                if (i*j>bigpalindrome)
-                    bigpalindrome=i*j;
+                if ( product > maxPalindrome)
+                {
+                    maxPalindrome = product;
+                }
             }
         }
     }
 
-    printf("Largest palindrome: \033[1m%i\033[0m\n", bigpalindrome);
+    printf("Largest palindromic product: %d\n", maxPalindrome);
 
     return 0;
 }
