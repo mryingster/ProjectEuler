cb535ee9799ec1bdd6971493d900b7298e00e2d2
Michael Caldwell <michaelc@yingster.net>
2014-09-30 12:41:26 -0700

    Simplifying solution, and cleaning up code. Removing escape characters from result text.

--- problem_002.c
+++ problem_002.c
@@ -1,35 +1,28 @@
 // -*- compile-command: "gcc -o problem_002 problem_002.c -Wall" -*-
 // Copyright (c) 2014 Michael Caldwell
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
     printf("Project Euler - Problem 2:\n"
            "By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.\n\n");
 
-    int i, previousNum=0, nextNum=0, currentNum=1, total;
+    int previousNum = 0, nextNum = 0, currentNum = 1;
+    int total = 0;
 
-    previousNum=0;
-    currentNum=1;
-    nextNum=0;
-
-    for (i=0; i<10000; i++)
+    while (true)
     {
-        nextNum=previousNum+currentNum;
-        previousNum=currentNum;
-        currentNum=nextNum;
-        if (currentNum < 4000000)
-        {
-            if ( currentNum % 2 )
-                printf("");
-            else
-                total=total+currentNum;
-        }
-        else
+        nextNum = previousNum + currentNum;
+        previousNum = currentNum;
+        currentNum = nextNum;
+        if ( currentNum >= 4000000 )
             break;
-      }
+        if ( currentNum % 2 == 0)
+            total += currentNum;
+       }
 
-    printf("Sum: \033[1m%i\033[0m\n", total);
+    printf("Sum: %d\n", total);
 
     return 0;
 }
