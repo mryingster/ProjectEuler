6e6f4178104035e88ca9b5d39080f5cf606277e6
Michael Caldwell <michaelc@yingster.net>
2014-10-13 20:45:07 -0700

    Adding test for isHexagonal() function Adding function for printing so that output is nicely lined up.

--- ceuler_test.c
+++ ceuler_test.c
@@ -4,13 +4,19 @@
 #include <stdio.h>
 #include "ceuler.h"
 
+void printTest(char name[])
+{
+    printf("Function: %-18s ", name);
+    return;
+}
+
 int main()
 {
     int number = 0;
     int count = 0;
 
     // Test prime calculation
-    printf("Testing isPrime() function: ");
+    printTest("isPrime()");
     for (number = 2 ; number < 1000 ; number++)
     {
         if (isPrime(number) == true)
@@ -22,7 +28,7 @@ int main()
         printf("Fail\n");
 
     // Test optimized prime calculation
-    printf("Testing isPrimeOpt() function: ");
+    printTest("isPrimeOpt()");
     int primeArray[15]={2,3,5,7,11,13,17,19,23,29,31,37,41,43,47};
     if (isPrimeOpt(53, primeArray) == true && isPrimeOpt(42, primeArray) == false)
         printf("Pass\n");
@@ -30,14 +36,14 @@ int main()
         printf("Fail\n");
 
     // Test palindromic calculation
-    printf("Testing isPalindromic() function: ");
+    printTest("isPalindromic()");
     if (isPalindromic(12321) == true && isPalindromic(123456) == false)
         printf("Pass\n");
     else
         printf("Fail\n");
 
     // Test pandigital number
-    printf("Testing isPandigital() function: ");
+    printTest("isPandigital()");
     if (isPandigital(951628437) == true && isPandigital(123455789) == false)
         printf("Pass\n");
     else
@@ -45,7 +51,7 @@ int main()
 
     // Test triangle number
     count = 0;
-    printf("Testing isTriangle() function: ");
+    printTest("isTriangle()");
     for (number=0 ; number<=10000; number++)
         if (isTriangle(number) == true)
             count++;
@@ -56,7 +62,7 @@ int main()
 
     // Test pentagonal number
     count = 0;
-    printf("Testing isPentagonal() function: ");
+    printTest("isPentagonal()");
     for (number=0 ; number<=10000; number++)
         if (isPentagonal(number) == true)
             count++;
@@ -65,36 +71,47 @@ int main()
     else
         printf("Fail\n");
 
+    // Test hexagonal number
+    count = 0;
+    printTest("isHexagonal()");
+    for (number=0 ; number<=10000; number++)
+        if (isHexagonal(number) == true)
+            count++;
+    if ( count == 70 )
+        printf("Pass\n");
+    else
+        printf("Fail\n");
+
     // Test digit factorial
-    printf("Testing digitFactorial() function: ");
+    printTest("digitFactorial()");
     if (digitFactorial(9) == 362880 && digitFactorial(10) == 0)
         printf("Pass\n");
     else
         printf("Fail\n");
 
     // Test power check
-    printf("Testing powerOf() function: ");
+    printTest("powerOf()");
     if (powerOf(2048, 2) == true && powerOf(73, 12) == false)
         printf("Pass\n");
     else
         printf("Fail\n");
 
     // Test Number Of Digits
-    printf("Testing numberOfDigits() function: ");
+    printTest("numberOfDigits()");
     if (numberOfDigits(12345678901) == 11 && numberOfDigits(0) == 1 && numberOfDigits(-11568) == 5)
         printf("Pass\n");
     else
         printf("Fail\n");
 
     // Test even number detection
-    printf("Testing isEven() function: ");
+    printTest("isEven()");
     if (isEven(10) == true && isEven(9) == false)
         printf("Pass\n");
     else
         printf("Fail\n");
 
     // Test word score
-    printf("Testing wordScore() function: ");
+    printTest("wordScore()");
     if (wordScore("ABC", 3) == 6 && wordScore("", 0) == 0)
         printf("Pass\n");
     else
