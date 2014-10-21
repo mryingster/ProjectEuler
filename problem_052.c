f4b374b8fa176535c892926f12a2c3349bce4537
Michael Caldwell <michaelc@yingster.net>
2014-10-21 07:32:18 -0700

    Adding solution for problem 52 in c.

---
+++ problem_052.c
@@ -0,0 +1,39 @@
+// -*- compile-command: "gcc -o problem_052 problem_052.c ceuler.c -Wall -lm" -*-
+// Copyright (c) 2014 Michael Caldwell
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ceuler.h"
+
+unsigned int numSignature(int number)
+{
+    int out = 0;
+    while (number > 0)
+    {
+        out += 1 * pow(10, number % 10);
+        number /= 10;
+    }
+    return out;
+}
+
+int main()
+{
+    printf("Project Euler - Problem 52:\n"
+           "Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits.\n\n");
+
+    int number = 1;
+
+    while (number++ < 1000000)
+    {
+        int checkSig = numSignature(number);
+        if (checkSig != numSignature(number * 2)) continue;
+        if (checkSig != numSignature(number * 3)) continue;
+        if (checkSig != numSignature(number * 4)) continue;
+        if (checkSig != numSignature(number * 5)) continue;
+        if (checkSig != numSignature(number * 6)) continue;
+        break;
+    }
+
+    printf("Smallest Number: %d\n", number);
+    return 0;
+}
