f6f00d2293db4ab8fff1171edf37f60185be0c1d
Michael Caldwell <michaelc@yingster.net>
2014-11-04 13:42:17 -0800

    Adding solution for problem 1 in lua.

---
+++ problem_001.lua
@@ -0,0 +1,20 @@
+#!/usr/bin/lua
+
+print("Project Euler - Problem 1")
+print("Find the sum of all the multiples of 3 or 5 below 1000.\n")
+
+number = 1
+sum = 0
+
+while (number < 1000) do
+   if (number % 3 == 0) then
+      sum = sum + number
+   else
+      if (number % 5 == 0) then
+         sum = sum + number
+      end
+   end
+   number = number + 1
+end
+
+print("Sum: "..sum)
