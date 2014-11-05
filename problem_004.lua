07a1bfbd0e649bca0b4613b79c3fbeb31514da98
Michael Caldwell <michaelc@yingster.net>
2014-11-05 08:58:59 -0800

    Adding solution to problem 4 in lua.

---
+++ problem_004.lua
@@ -0,0 +1,27 @@
+#!/usr/bin/lua
+
+print("Project Euler - Problem 4:")
+print("Find the largest palindrome made from the product of two 3-digit numbers.\n")
+
+result = 0
+
+for a = 999, 100, -1 do
+   for b = a, 100, -1 do
+      product = a * b
+      tcudorp = 0
+      while (product > 0) do
+         tcudorp = math.floor(tcudorp * 10)
+         tcudorp = tcudorp + (product % 10)
+         product = math.floor(product / 10)
+      end
+      product = a * b
+      --print (product.." "..tcudorp) --DEBUG
+      if ( product == tcudorp ) then
+         if result < product then
+            result = product
+         end
+      end
+   end
+end
+
+print("Largest product: "..result)
