60cdacec559f2e3da7309c762760187299d17550
Michael Caldwell <michaelc@yingster.net>
2014-11-06 23:05:10 -0800

    Redoing the main algorithm to be more efficient.

--- problem_009.c
+++ problem_009.c
@@ -1,37 +1,36 @@
-// -*- compile-command: "gcc -o problem_009 problem_009.c -Wall" -*-
+// -*- compile-command: "gcc -o problem_009 problem_009.c -Wall -lm" -*-
 // Copyright (c) 2014 Michael Caldwell
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
     const int target = 1000;
-    int product = 0;
-    int a, b, c;
 
     printf("Project Euler - Problem 9:\n"
            "There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.\n\n");
 
-    for (a=1 ; a<target/2 ; a++)
+
+    int product = 0;
+    int a = 1;
+    while (a++)
     {
-        for (b=a ; b<target/2 ; b++)
+        int b = a;
+        while (b++)
         {
-            for (c=b ; c<target ; c++)
+            int c = sqrt(a*a + b*b);
+            int sum = a + b + c;
+            if (sum > target) break;
+            if (c != sqrt(a*a + b*b)) continue;
+            if (sum == target)
             {
-                if (a*a + b*b == c*c && a + b + c == target)
-                {
-                    //printf("Found Match. %d %d %d %i\n", a, b, c, a*b*c); //DEBUG
-                    product = a*b*c;
-                    break;
-                }
-            }
-            if (product != 0)
+                product = a * b * c;
                 break;
+            }
         }
-        if (product != 0)
-            break;
+        if (product != 0) break;
     }
 
     printf("Product: %d\n", product);
-
     return 0;
 }
