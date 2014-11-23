7e621973866f98eac410c29f1a53e91a894acfd4
Michael Caldwell <michaelc@yingster.net>
2014-11-22 21:22:09 -0800

    Returning solution as string instead of as unsigned long. Using cout instead of printf statements. Other minor things.

--- problem_068.cpp
+++ problem_068.cpp
@@ -1,24 +1,24 @@
-// -*- compile-command: "g++ -o problem_068 problem_068.c ceuler.c -Wall -lm" -*-
+// -*- compile-command: "g++ -std=c++0x -o problem_068 problem_068.cpp ceuler.c -Wall -lm" -*-
 // Copyright (c) 2014 Michael Caldwell
 #include <stdio.h>
 #include <stdbool.h>
 #include <algorithm>
+#include <string>
+#include <iostream>
 #include "ceuler.h"
 
-unsigned long long checkSolution(const int o[], const int i[])
+std::string checkSolution(const int o[], const int i[], int length)
 {
+    std::string result;
     int target = o[0] + i[0] + i[1];
-    unsigned long long result = 0;
 
-    for (int p=0; p<5; p++)
+    //Compare each triplet's sum against the first
+    for (int p=0; p<length; p++)
     {
-        int check[] = {o[p], i[p], i[(p+1)%5]};
-        if (check[0] + check[1] + check[2] != target) return 0;
+        int check[] = {o[p], i[p], i[(p+1)%length]};
+        if (check[0] + check[1] + check[2] != target) return "";
         for (int n=0; n<3; n++)
-        {
-            result *= pow(10, numberOfDigits(check[n]));
-            result += check[n];
-        }
+            result += std::to_string(check[n]);
     }
 
     return result;
@@ -26,26 +26,24 @@ unsigned long long checkSolution(const int o[], const int i[])
 
 int main()
 {
-    printf("Project Euler - Problem 68:\n"
-           "Find the maximum concatenation for a 16 digit string for a solution to a \"magic 5-gon ring\" using the numbers 1-10?\n\n");
+    std::cout << "Project Euler - Problem 68:" << std::endl;
+    std::cout << "Find the maximum concatenation for a 16 digit string for a solution to a \"magic 5-gon ring\" using the numbers 1-10?" << std::endl << std::endl;
 
     //Do permutations and check answer
-    unsigned long long solution = 0;
+    std::string solution;
     int outer[] = {6, 7, 8, 9, 10};
     int length = sizeof(outer)/sizeof(outer[0]);
     while (std::next_permutation(outer, outer+length))
     {
         int inner[] = {1, 2, 3, 4, 5};
-        //printf("%d, %d, %d, %d, %d\n", outer[0], outer[1], outer[2], outer[3], outer[4]); //DEBUG
         while (std::next_permutation(inner, inner+length))
         {
-            //printf("%d, %d, %d, %d, %d\n", inner[0], inner[1], inner[2], inner[3], inner[4]); //DEBUG
-            solution = checkSolution(outer, inner);
-            if (solution != 0) break;
+            solution = checkSolution(outer, inner, length);
+            if (!solution.empty()) break;
         }
-        if (solution != 0) break;
+        if (!solution.empty()) break;
     }
 
-    printf("Solution, %llu!\n", solution);
+    std::cout << "Solution: " << solution << std::endl;
     return 0;
 }
