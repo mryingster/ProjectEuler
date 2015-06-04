634eb798b27654a3b16de9907ad1e8f69a82ede9
Michael Caldwell <michaelc@yingster.net>
2015-06-04 09:31:47 -0700

    Refining solution by applying filters for observed pattern to prevent unessesary searching. Result now takes around 2 minutes (down from an original 4 hours).

--- problem_357.c
+++ problem_357.c
@@ -1,11 +1,12 @@
-// -*- compile-command: "gcc -std=c99 -o problem_357 problem_357.c -Wall ceuler.c -lm" -*-
+// -*- compile-command: "gcc -std=c99 -o problem_357 problem_357.c -Wall ceuler.c -lm -O3" -*-
 // Copyright (c) 2015 Michael Caldwell
 #include <stdio.h>
+#include <stdlib.h>
 #include "ceuler.h"
 
 bool isPrimeGen(int n)
 {
-    for (int d=1; d<sqrt(n)+1; d++)
+    for (int d=1; d<=sqrt(n); d++)
         if (n % d == 0)
             if (isPrime(d+(n/d)) != true)
                 return false;
@@ -26,16 +27,27 @@ int main()
     unsigned long long sum = 1;
     bool debug = false;
 
-    for (int n=2; n<=limit; n+=2)
-        if (isPrimeGen(n))
+    // Now solve
+    int candidate = 0;
+    for (int n=0; candidate<limit; n++)
+    {
+        candidate = 4 * n + 2;             // Pattern 1
+        if (isPrime(candidate + 1))        // Pattern 2
         {
-            sum += n;
-            if (debug == true)
+            if (isPrime(candidate/2+2))    // Pattern 3
             {
-                printf("%d, %llu\r", n, sum);
-                fflush(stdout);
+                if (isPrimeGen(candidate)) // Verify number by brute force
+                {
+                    sum += candidate;
+                    if (debug == true)
+                    {
+                        printf("%d, %llu\r", candidate, sum);
+                        fflush(stdout);
+                    }
+                }
             }
         }
+    }
 
     printf("Sum: %llu     \n", sum);
     printElapsedTime(start);
