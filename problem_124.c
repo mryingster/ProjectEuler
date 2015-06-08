60c9049a43a622b83d67e22c20b40f7e1963ead5
Michael Caldwell <michaelc@yingster.net>
2015-06-08 10:01:57 -0700

    Refining solution to use a struct instead of using 2 arrays. More direct. Adding timing code from ceuler.

--- problem_124.c
+++ problem_124.c
@@ -4,11 +4,17 @@
 #include <stdlib.h>
 #include "ceuler.h"
 
+typedef struct rad rad;
+struct rad{
+    int n;
+    int rad;
+};
+
 int compare(const void *a, const void *b)
 {
-    const int *ia = (const int *)a;
-    const int *ib = (const int *)b;
-    return *ia - *ib;
+    const rad *ia = (const rad *)a;
+    const rad *ib = (const rad *)b;
+    return ia->rad - ib->rad;
 }
 
 int factorProduct(int number, const int * primes, const int primeIndex)
@@ -24,7 +30,6 @@ int factorProduct(int number, const int * primes, const int primeIndex)
             number /= primes[pindex];
             if (++count == 1)
                 product *= primes[pindex];
-
         }
         else
         {
@@ -40,59 +45,42 @@ int main()
     printf("Project Euler - Problem 124:\n"
            "Find the 10,000th n in a sorted list of rad(n) where rad(n) is the product of the distinct prime factors of n.\n\n");
 
+    // Begin time tracking
+    struct timeval start;
+    gettimeofday(&start, NULL);
+
     const int limit = 100001;
-    int rad[100000] = {};
+    rad rad[100001] = {};
     int primes[100000] = {};
     int pindex = 0;
     bool debug = false;
 
-    rad[1] = 1;
+    rad[1].n   = 1;
+    rad[1].rad = 1;
     for (int n=2; n<limit; n++)
     {
+        rad[n].n = n;
         if (isPrime(n))
         {
-            rad[n] = n;
+            rad[n].rad = n;
             primes[pindex++] = n;
         }
         else
         {
-            rad[n] = factorProduct(n, primes, pindex);
+            rad[n].rad = factorProduct(n, primes, pindex);
         }
     }
-
-    // Duplicate array for later
-    int rad2[100000] = {};
-    for (int i=0; i<limit; i++)
-        rad2[i] = rad[i];
 
     // QSORT them here...
     qsort(rad, limit, sizeof(rad[0]), compare);
 
-    // Find offset (number of elements preceeding with same value)
-    int answer = rad[10000];
-    int offset = 0;
-    while (rad[10000-offset] == answer)
-        offset++;
-
-    // Go back to original list to find index of answer from sorted list
-    int finalAnswer = 0;
-    for (int i=0; i<limit; i++)
-    {
-        if (rad2[i] == answer)
-            offset--;
-        if (offset == 0)
-        {
-            finalAnswer = i;
-            break;
-        }
-    }
-
-    // DEBUG
+    // DEBUG - Print structure
     if (debug == true)
         for (int n=1; n<limit; n++)
-            printf("%d, %d\n", n, rad2[n]);
+            printf("%d, %d\n", rad[n].n, rad[n].rad);
 
-    printf("%d\n", finalAnswer);
+    printf("%d\n", rad[10000].n);
+    printElapsedTime(start);
 
     return 0;
 }
