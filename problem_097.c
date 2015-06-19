b4fba9b3b5d0a0e4d196ac6452710f324f8bda55
Michael Caldwell <michaelc@yingster.net>
2015-06-19 09:12:50 -0700

    Redoing solution in more straightfoward (and faster) manner by truncating a 64 bit number rather than dealing with an array.

--- problem_097.c
+++ problem_097.c
@@ -12,43 +12,24 @@ int main()
     struct timeval start;
     gettimeofday(&start, NULL);
 
-    int array[10] = {0};
-    int exponent = 7830457; // 2's exponent
-    array[9]=28433;         // Starting value
-    bool debug = false;
+    unsigned long long n = 2;
+    unsigned long long max = 10000000000;  // 10 digits
+    int exponent = 7830457;                // 2's exponent
+    int base = 28433;                      // Starting value
 
-    //Perform exponent multiplication
-    for (int i=1; i<=exponent; i++)
+    while (exponent > 1)
     {
-        // Multiply each element by 2
-        for (int n=0; n<10; n++)
-            array[n] *= 2;
-
-        // Carry over number for big cells
-        for (int j=9; j>0; j--)
-            while (array[j] >= 10)
-            {
-                array[j-1]++;
-                array[j] -= 10;
-            }
-        while (array[0] >= 10)
-            array[0] -= 10;
-
-        // Debug - Print array
-        if (debug == true)
-        {
-            for (int j=0; j<10; j++)
-                printf("%d", array[j]);
-            printf("\n");
-        }
+        n *= 2;
+        exponent--;
+        while (n > max) n -= max;
     }
 
-    array[9]++;  // Add one!
+    n *= base;
+    n++;
 
-    //Print array
-    for (int j=0; j<10; j++)
-        printf("%d", array[j]);
-    printf("\n");
+    while (n > max) n -= max;
+
+    printf("%llu\n", n);
     printElapsedTime(start);
 
     return 0;
