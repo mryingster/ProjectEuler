611baf025a0203efd0cf4139c752d0478818dd35
Michael Caldwell <michaelc@yingster.net>
2015-07-15 10:09:58 -0700

    Switching to primeSieve function. Adding timer for solution.

--- problem_010.c
+++ problem_010.c
@@ -1,28 +1,29 @@
-// -*- compile-command: "gcc -o problem_010 problem_010.c ceuler.c -Wall -lm" -*-
+// -*- compile-command: "gcc -std=c99 -o problem_010 problem_010.c ceuler.c -Wall -lm" -*-
 // Copyright (c) 2014 Michael Caldwell
 #include <stdio.h>
 #include "ceuler.h"
 
 int main()
 {
-    int index  = 0;
-    int number = 0;
-    int primes[500000] = {0};
-    unsigned long long sum = 0;
-
     printf("Project Euler - Problem 10:\n"
            "Find the sum of all the primes below two million.\n\n");
 
-    for (number=2 ; number<2000000 ; number++)
-        if (isPrimeOpt(number, primes))
-	{
-            sum += number;
-            primes[index] = number;
-            index++;
-            //printf("prime:%i Total:%llu\n", i, sum); //DEBUG
-	}
+    // Begin time tracking
+    struct timeval start;
+    gettimeofday(&start, NULL);
+
+    int limit = 2000000;
+    int nprimes = 1.5 * limit / log(limit);    // Estimate number of primes under limit
+    int *primes = malloc(nprimes*sizeof(int));
+    unsigned long long sum = 0;
+
+    primeSieve(primes, nprimes);
+
+    for (int i=0; primes[i]<limit; i++)
+        sum += primes[i];
 
     printf("Sum of primes: %llu\n", sum);
+    printElapsedTime(start);
 
     return 0;
 }
