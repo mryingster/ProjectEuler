28721458f729dadda7f2d4806f257904cac19bfa
Michael Caldwell <michaelc@yingster.net>
2015-07-15 10:15:47 -0700

    Changing solution to use primeSieve function. Adding timer to solution.

--- problem_012.c
+++ problem_012.c
@@ -1,18 +1,18 @@
-// -*- compile-command: "gcc -o problem_012 problem_012.c ceuler.c -Wall -lm" -*-
+// -*- compile-command: "gcc -std=c99 -o problem_012 problem_012.c ceuler.c -Wall -lm" -*-
 // Copyright (c) 2014 Michael Caldwell
 #include <stdio.h>
 #include "ceuler.h"
 
-int findDivisors(int input, int *primes, int primesToFind)
+int findDivisors(int input, int *primes, int nprimes)
 {
     int index, count=1;
-    int primeCount[primesToFind];
-    for (index=0; index<primesToFind; index++)
+    int primeCount[nprimes];
+    for (index=0; index<nprimes; index++)
         primeCount[index] = 0;
 
     // Prime factorization
     while (input != 1)
-        for (index=0; index<primesToFind; index++)
+        for (index=0; index<nprimes; index++)
             if (input % primes[index] == 0)
             {
                 input = input / primes[index];
@@ -20,7 +20,7 @@ int findDivisors(int input, int *primes, int primesToFind)
             }
 
     // Convert prime factorization to divisors
-    for (index=0; index<primesToFind; index++)
+    for (index=0; index<nprimes; index++)
         if (primeCount[index] > 0)
             count *= (primeCount[index]+1);
 
@@ -29,30 +29,17 @@ int findDivisors(int input, int *primes, int primesToFind)
 
 int main()
 {
-    int index=0, triangle=0, divisors=0;
-    int primeCount = 0;
-
-    int primesToFind = 1500;
-    int primes[primesToFind];
-
     printf("Project Euler - Problem 12:\n"
            "What is the value of the first triangle number to have over five hundred divisors?\n\n");
 
-    // Initialize primesToFind
-    for (index=0; index<primesToFind; index++)
-        primes[index] = 0;
+    // Begin time tracking
+    struct timeval start;
+    gettimeofday(&start, NULL);
 
-    // Pre-Calculate Primes
-    index = 1;
-    while (primeCount < primesToFind)
-    {
-        index++;
-        if (isPrimeOpt(index, primes))
-        {
-            primes[primeCount]=index;
-            primeCount++;
-        }
-    }
+    int index=0, triangle=0, divisors=0;
+    int nprimes = 1500;
+    int primes[nprimes];
+    primeSieve(primes, nprimes);
 
     // Calculate Triangles
     index = 0;
@@ -64,12 +51,14 @@ int main()
         // Skip numbers not divisible by 3 or 5
         if ( (triangle % 3 != 0) || (triangle % 5 != 0) )
             continue;
-        divisors = findDivisors(triangle, primes, primesToFind);
+        divisors = findDivisors(triangle, primes, nprimes);
 
         if (divisors > 500)
             break;
     }
 
     printf("Triangle Value: %d, Divisors: %d\n", triangle, divisors);
+    printElapsedTime(start);
+
     return 0;
 }
