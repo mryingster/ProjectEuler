082c1435544298868d128820c253385cbe82d1c4
Michael Caldwell <michaelc@yingster.net>
2015-07-15 10:19:00 -0700

    Updating to use isPrime function. Adding timer for solution.

--- problem_007.c
+++ problem_007.c
@@ -1,22 +1,26 @@
-// -*- compile-command: "gcc -o problem_007 problem_007.c ceuler.c -Wall -lm" -*-
+// -*- compile-command: "gcc -std=c99 -o problem_007 problem_007.c ceuler.c -Wall -lm" -*-
 // Copyright (c) 2014 Michael Caldwell
 #include <stdio.h>
 #include "ceuler.h"
 
 int main()
 {
-    int primeCount    = 0;
-    int candidate     = 1;
-    int primes[10002] = {0};
-
     printf("Project Euler - Problem 7:\n"
            "What is the 10,001st prime number?\n\n");
 
-    while ( primeCount < 10001 )
-        if ( isPrimeOpt(++candidate, primes) )
-            primes[primeCount++] = candidate;
+    // Begin time tracking
+    struct timeval start;
+    gettimeofday(&start, NULL);
+
+    int primeCount    = 0;
+    int candidate     = 1;
+
+    while (primeCount < 10001)
+        if (isPrime(++candidate))
+            primeCount++;
 
     printf("10,001st prime: %d\n", candidate);
+    printElapsedTime(start);
 
     return 0;
 }
