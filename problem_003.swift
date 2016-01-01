1e92276e550f0f5cadcc14bfb0013e4a75d68213
Michael Caldwell <michaelc@yingster.net>
2016-01-01 09:49:58 -0800

    Adding solution for problem 3 in swift.

---
+++ problem_003.swift
@@ -0,0 +1,18 @@
+#!/usr/bin/xcrun swift
+// -*- compile-command: "swiftc problem_003.swift -o problem_003" -*-
+// Copyright (c) 2016 Michael Caldwell
+
+print("Project Euler - Problem 3:")
+print("What is the largest prime factor of the number 600851475143?\n")
+
+var n = 600851475143
+var d = 2
+
+while d < n {
+    if n % d == 0 {
+       n /= d
+    }
+    d++
+}
+
+print(d)
