a5280328df1845a502661be7c6755e28428a31c5
Michael Caldwell <michaelc@yingster.net>
2016-01-04 08:39:17 -0800

    Adding timing to solution using ceuler library.

--- problem_011.c
+++ problem_011.c
@@ -1,35 +1,10 @@
-// -*- compile-command: "gcc -o problem_011 problem_011.c -Wall" -*-
+// -*- compile-command: "gcc -std=c99 -o problem_011 problem_011.c ceuler.c -Wall -lm" -*-
 // Copyright (c) 2014 Michael Caldwell
 #include <stdio.h>
+#include "ceuler.h"
 
 int main()
 {
-    int y = 0, x = 0;
-    int largest = 0, candidate = 0;
-
-    printf("Project Euler - Problem 11:\n"
-           "What is the greatest product of four adjacent numbers in any direction (up, down, left, right, or diagonally) in the 20x20 grid?\n\n"
-           "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08\n"
-           "49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00\n"
-           "81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65\n"
-           "52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91\n"
-           "22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80\n"
-           "24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50\n"
-           "32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70\n"
-           "67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21\n"
-           "24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72\n"
-           "21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95\n"
-           "78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92\n"
-           "16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57\n"
-           "86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58\n"
-           "19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40\n"
-           "04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66\n"
-           "88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69\n"
-           "04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36\n"
-           "20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16\n"
-           "20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54\n"
-           "01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48\n\n");
-
     int grid[20][20]={
         { 8, 2,22,97,38,15, 0,40, 0,75, 4, 5, 7,78,52,12,50,77,91, 8},
         {49,49,99,40,17,81,18,57,60,87,17,40,98,43,69,48, 4,56,62, 0},
@@ -52,51 +27,75 @@ int main()
         {20,73,35,29,78,31,90, 1,74,31,49,71,48,86,81,16,23,57, 5,54},
         {01,70,54,71,83,51,54,69,16,92,33,48,61,43,52, 1,89,19,67,48}
     };
-
     const unsigned long gridYSize = sizeof(grid[0]) / sizeof(int);
     const unsigned long gridXSize = sizeof(grid) / sizeof(int) / gridYSize;
+
+    printf("Project Euler - Problem 11:\n"
+           "What is the greatest product of four adjacent numbers in any direction (up, down, left, right, or diagonally) in the 20x20 grid?\n\n");
+
+    // Print grid for problem description
+    for (int y=0 ; y<gridYSize ; y++)
+    {
+        for (int x=0 ; x<gridXSize ; x++)
+            printf("%02d ", grid[y][x]);
+        printf("\n");
+    }
+    printf("\n");
+
+    // Begin time tracking
+    struct timeval start;
+    gettimeofday(&start, NULL);
+
+    int largest = 0, candidate = 0;
     const int searchSize = 4;
+    bool debug = false;
 
     //Horizontal
-    for (y=0 ; y<gridYSize ; y++)
-        for (x=0 ; x<=gridXSize - searchSize ; x++)
+    for (int y=0 ; y<gridYSize ; y++)
+        for (int x=0 ; x<=gridXSize - searchSize ; x++)
         {
             candidate = grid[y][x] * grid[y][x+1] * grid[y][x+2] * grid[y][x+3];
-            //printf("%i\n", candidate);
+            if (debug == true)
+                printf("%d\n", candidate);
             if (candidate > largest)
                 largest = candidate;
         }
 
     //Vertical
-    for (y=0 ; y<=gridYSize - searchSize ; y++)
-        for (x=0 ; x<gridXSize ; x++)
+    for (int y=0 ; y<=gridYSize - searchSize ; y++)
+        for (int x=0 ; x<gridXSize ; x++)
         {
             candidate = grid[y][x] * grid[y+1][x] * grid[y+2][x] * grid[y+3][x];
-            //printf("%i\n", candidate);
+            if (debug == true)
+                printf("%d\n", candidate);
             if (candidate > largest)
                 largest = candidate;
         }
 
     //Diagonal down right
-    for (y=0 ; y<=gridYSize - searchSize ; y++)
-        for (x=0 ; x<=gridXSize - searchSize ; x++)
+    for (int y=0 ; y<=gridYSize - searchSize ; y++)
+        for (int x=0 ; x<=gridXSize - searchSize ; x++)
         {
             candidate = grid[y][x] * grid[y+1][x+1] * grid[y+2][x+2] * grid[y+3][x+3];
-            //printf("%i\n", candidate);
+            if (debug == true)
+                printf("%d\n", candidate);
             if (candidate > largest)
                 largest = candidate;
         }
 
     //Diagonal down left
-    for (y=3 ; y<gridYSize ; y++)
-        for (x=0 ; x<=gridXSize - searchSize ; x++)
+    for (int y=searchSize-1 ; y<gridYSize ; y++)
+        for (int x=0 ; x<=gridXSize - searchSize ; x++)
         {
             candidate = grid[y][x] * grid[y-1][x+1] * grid[y-2][x+2] * grid[y-3][x+3];
-            //printf("%i\n", candidate);
+            if (debug == true)
+                printf("%d\n", candidate);
             if (candidate > largest)
                 largest = candidate;
         }
 
     printf("Greatest product: %d\n", largest);
+    printElapsedTime(start);
+
     return 0;
 }
