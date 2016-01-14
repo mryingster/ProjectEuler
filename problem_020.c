1bbc2c07d284829036cd5391d605f0edad6d94be
Michael Caldwell <michaelc@yingster.net>
2016-01-14 08:59:23 -0800

    Updating solution with time tracking. Improved debug prints.

--- problem_020.c
+++ problem_020.c
@@ -1,45 +1,53 @@
-// -*- compile-command: "gcc -o problem_020 problem_020.c -Wall" -*-
+// -*- compile-command: "gcc -std=c99 -o problem_020 problem_020.c ceuler.c -Wall -lm" -*-
 // Copyright (c) 2014 Michael Caldwell
 #include <stdio.h>
+#include "ceuler.h"
 
 int main()
 {
     printf("Project Euler - Problem 20:\n"
            "Find the sum of the digits in the number 100!\n\n");
 
+    // Begin time tracking
+    struct timeval start;
+    gettimeofday(&start, NULL);
+
     int bigNum[1000] = {0};
-    int index = 0;
-    int factorial = 0;
-    int sum = 0;
+    bool debug = false;
 
     bigNum[997]=1; // Starting value is 100
 
-    for (factorial=99 ; factorial>0 ; factorial--)
+    for (int factorial=99 ; factorial>0 ; factorial--)
     {
         // Multiply by factorial
-        for (index=999 ; index>=0 ; index--)
-            bigNum[index] *= factorial;
+        for (int i=999 ; i>=0 ; i--)
+            bigNum[i] *= factorial;
 
         // Carry-over each element if needed
-        for (index=999 ; index>=0 ; index--)
-            while (bigNum[index] > 9)
+        for (int i=999 ; i>=0 ; i--)
+            while (bigNum[i] > 9)
             {
-                bigNum[index]   -= 10;
-                bigNum[index-1] += 1;
+                bigNum[i]   -= 10;
+                bigNum[i-1] += 1;
             }
 
-        /*
-        // DEBUG - Print each element
-        for (index=0 ; index<1000 ; index++)
-            printf("%i",bigNum[index]);
-        printf("\n");
-        */
+        // DEBUG - Print result
+        if (debug == true)
+        {
+            int i = 0;
+            while (bigNum[++i] == 0);
+            for (; i<1000 ; i++)
+                printf("%d", bigNum[i]);
+            printf("\n");
+        }
     }
 
-    for (index=0 ; index<1000 ; index++)
-        sum += bigNum[index];
+    int sum = 0;
+    for (int i=0 ; i<1000 ; i++)
+        sum += bigNum[i];
 
-    printf("Sum: %i\n", sum);
+    printf("%d\n", sum);
+    printElapsedTime(start);
 
     return 0;
 }
