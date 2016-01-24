e9d718d60f1941772a792eddc365d97051be05ca
Michael Caldwell <michaelc@yingster.net>
2016-01-23 23:25:01 -0800

    Adding timing to problem 21.

--- problem_021.c
+++ problem_021.c
@@ -1,7 +1,7 @@
-// -*- compile-command: "gcc -o problem_021 problem_021.c -Wall" -*-
+// -*- compile-command: "gcc std=c99 -o problem_021 problem_021.c ceuler.c -Wall -lmxs" -*-
 // Copyright (c) 2014 Michael Caldwell
 #include <stdio.h>
-#include <math.h>
+#include "ceuler.h"
 #include <assert.h>
 
 int main()
@@ -9,23 +9,23 @@ int main()
     printf("Project Euler - Problem 21:\n"
            "Evaluate the sum of all the amicable numbers under 10000.\n\n");
 
-    int number     = 0;
+    // Begin time tracking
+    struct timeval start;
+    gettimeofday(&start, NULL);
+
     int divisorSum = 0;
-    int divisor    = 0;
     int sum        = 0;
     short history[10001] = {0};
+    bool debug = false;
 
-    for (number=1 ; number<10001 ; number++)
+    for (int number=1 ; number<10001 ; number++)
     {
         divisorSum=0;
 
         // Find sum of divisors
-        for (divisor=1 ; divisor<number ; divisor++)
+        for (int divisor=1 ; divisor<number/2+1 ; divisor++)
             if (number % divisor == 0)
-            {
-                //printf("%d divisible by %d\n", number, divisor); // DEBUG
                 divisorSum += divisor;
-            }
 
         // Ignore sums larger than 10000
         if ( divisorSum > 10000 )
@@ -34,15 +34,16 @@ int main()
         // Check if amicable
         if (history[divisorSum] == number)
         {
-            //printf("Amicable pair %d %d\n", number, divisorSum); // DEBUG
+            if (debug == true) printf("Amicable pair %d %d\n", number, divisorSum); // DEBUG
             sum += number + divisorSum;
         }
 
         // Record divisor sum
-        history[number]=divisorSum;
+        history[number] = divisorSum;
     }
 
     printf("Sum: %d\n", sum);
+    printElapsedTime(start);
 
     return 0;
 }
