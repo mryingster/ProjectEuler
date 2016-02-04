d461079ffbcd976a08691ba25cd8f80a1166d9f4
Michael Caldwell <michaelc@yingster.net>
2016-02-04 15:59:19 -0800

    Speeding solution by using lookup table for values instead of calculating powers repeatedly.

--- problem_030.py
+++ problem_030.py
@@ -3,13 +3,18 @@
 print("Project Euler - Problem 30:")
 print("Find the sum of all the numbers that can be written as the sum of fifth powers of their digits\n")
 
+exp = 5
+limit = 6*9 ** exp
+dict={"0":0**exp, "1":1**exp, "2":2**exp, "3":3**exp,
+      "4":4**exp, "5":5**exp, "6":6**exp, "7":7**exp,
+      "8":8**exp, "9":9**exp}
+
 answer = 0
-limit = 6*9 ** 5
 for n in range(2, limit):
     s = 0
     for d in str(n):
-        s += int(d)**5
+        s += dict[d]
     if n == s:
         answer += n
 
-print answer
+print(answer)
