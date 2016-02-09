c1a4c9c7377bf71ca8e3e65ec8795eee101c4afd
Michael Caldwell <michaelc@yingster.net>
2016-02-09 10:12:27 -0800

    Improving solution for problem 27 in C. Adding timing and use of ceuler's prime function.

--- problem_027.c
+++ problem_027.c
@@ -1,99 +1,45 @@
-// -*- compile-command: "gcc -o problem_027 problem_027.c -Wall" -*-
+// -*- compile-command: "gcc -std=c99 -o problem_027 problem_027.c ceuler.c -Wall -lm" -*-
 // Copyright (c) 2014 Michael Caldwell
 #include <stdio.h>
-#include <math.h>
-
-int isPrime(int input, int *primes)
-{
-    int n=0;
-
-    if (input == 2)
-        return 1;
-    else
-        while (primes[n] != 0)
-        {
-            if (input % primes[n] == 0)
-                return 0;
-            n++;
-        }
-    return 1;
-}
-
+#include "ceuler.h"
 int main()
 {
     printf("Project Euler - Problem 27:\n"
            "Find the product of the coefficients, a and b, for the quadratic expression, n^2 + an + b, that produces the maximum number of primes for consecutive values of n, starting with n = 0.\n\n");
 
-    int a=0, b=0, n=0;
-    int primeCache[500000] = {};
-    int count, candidate;
-    int candidate_elements[10] = {};
+    // Begin time tracking
+    struct timeval start;
+    gettimeofday(&start, NULL);
 
-    primeCache[0]=2;
-
-    for (a=-1000 ; a<1000 ; a++)
+    int answer[3] = {};
+    for (int a=-1000 ; a<1000 ; a++)
     {
-        for (b=-1000 ; b<1000 ; b++)
+        for (int b=-1000 ; b<1000 ; b++)
 	{
-            count=0;
-            for (n=0 ; n<100 ; n++)
+            int count = 0;
+            for (int n=0 ; n<100 ; n++)
 	    {
-                // Formula: n^2 + an + b
-                // Use formula to get answer
-                int answer=(n*n)+(n*a)+b;
-
-                candidate=0;
-
-                // Check if prime
-                int index;
-                for (index=0 ; primeCache[index]<=answer ; index++)
-		{
-                    // Create additional primes in cache if needed
-                    if (primeCache[index+1] == 0)
-		    {
-                        int primesearch=primeCache[index];
-                        while (primeCache[index+1]==0)
-			{
-                            primesearch++;
-                            if (isPrime(primesearch, primeCache))
-			    {
-                                primeCache[index+1]=primesearch;
-                                break;
-			    }
-			}
-		    }
-
-                    // Actually check here
-                    if (primeCache[index] == answer)
-                        candidate=1;
-
-                    if (primeCache[index] > answer)
-                        break;
-		}
-
-                // If prime, count and go on
-                if (candidate==1)
-		{
+                // Use formula, n^2 + an + b, to get answer
+                int answer = (n*n)+(n*a)+b;
+                if (isPrime(answer) == true)
                     count++;
-
-                    // Keep track of best performers
-                    if (count > candidate_elements[0])
-		    {
-                        candidate_elements[0]=count;
-                        candidate_elements[1]=a;
-                        candidate_elements[2]=b;
-                        candidate_elements[3]=n;
-                        //printf("Best so far: a=%i, b=%i, produces %i primes (n=%i)\n", candidate_elements[1], candidate_elements[2], candidate_elements[0], candidate_elements[3]); //Debug
-		    }
-		}
                 else
-                    break; // Break if not a consecutive prime;
-	    }
+                    break;
+            }
+
+            // Keep track of best performers
+            if (count > answer[0])
+            {
+                answer[0]=count;
+                answer[1]=a;
+                answer[2]=b;
+            }
 	}
     }
 
     printf("Solution: a:%d, b:%d, Product: %d (produces %d primes)\n",
-           candidate_elements[1], candidate_elements[2], candidate_elements[1]*candidate_elements[2], candidate_elements[0]);
+           answer[1], answer[2], answer[1]*answer[2], answer[0]);
+    printElapsedTime(start);
 
     return 0;
 }
