6dd60c2a32e8c680d43dbaa7b5f5eb07fb5837d6
Michael Caldwell <michaelc@yingster.net>
2016-02-25 20:35:34 -0800

    Making problem solve with parallel processes. Got compute time down to 3 minutes.

--- problem_010.sh
+++ problem_010.sh
@@ -5,9 +5,8 @@ echo "Find the sum of all the primes below two million."
 echo
 
 limit=2000000
-sum=0
 
-# Generate primes up to sqrt of 2000000
+# Generate primes up to sqrt of limit
 primeLimit=$(echo "scale=0;sqrt($limit*2)" | bc) # To validate prime n, only need to check divisibility up to sqrt(n)
 primes[$primeLimit]=0                            # Prime table for our seive function
 primeList[0]=2                                   # Array of primes for finding more primes later
@@ -15,7 +14,6 @@ primeListSquared[0]=4                            # Array for squared primes to m
 primeIndex=0                                     # Index for recording found primes
 for (( i=2; i<$primeLimit; i++ )); do
     if [[ ${primes[$i]} -eq \0 ]]; then
-        sum=$(($sum+$i))
         primeList[$primeIndex]=$i
         primeListSquared[$primeIndex]=$i*$i
         primeIndex=$(($primeIndex+1))
@@ -25,22 +23,39 @@ for (( i=2; i<$primeLimit; i++ )); do
     fi
 done
 
-# Make sure primeLimit is an odd number
-[[ $(($primeLimit % 2)) -eq 0 ]] && primeLimit=$(($primeLimit + 1))
+function checkRange() {
+    start=$1
+    end=$2
+    rangeSum=0
 
-# Use known primes to check all remaining numbers up to limit
-for (( i=$primeLimit; i<$limit; i+=2 )); do
-    prime=1
-    for (( p=0; ${primeListSquared[$p]}<=$i; p++ )); do
-        if [[ $(($i % ${primeList[$p]})) -eq 0 ]]; then
-            prime=0
-            break
+    # Make sure range starts at an odd number
+    [[ $start -lt 2 ]] && start=3 && rangeSum=2
+    [[ $(($start % 2)) -eq 0 ]] && start=$(($start + 1))
+
+    # Use known primes to check all remaining numbers up to limit
+    for (( i=$start; i<$end; i+=2 )); do
+        prime=1
+        for (( p=0; ${primeListSquared[$p]}<=$i; p++ )); do
+            if [[ $(($i % ${primeList[$p]})) -eq 0 ]]; then
+                prime=0
+                break
+            fi
+        done
+        # Record!
+        if [[ $prime -eq 1 ]]; then
+            rangeSum=$(($rangeSum+$i))
         fi
     done
-    # Record!
-    if [[ $prime -eq 1 ]]; then
-        sum=$(($sum+$i))
-    fi
-done
+    echo $rangeSum
+}
 
-echo $sum
+# Split up and check with multiple processes
+sum=0
+range=$(($limit / 50))
+for (( start=0; start<$limit; start+=$range )); do
+    checkRange $start $(($start+$range)) &
+done | while read result; do
+    sum=$(($sum+$result))
+    echo $sum
+done | tail -1
+
