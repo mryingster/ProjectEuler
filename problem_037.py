6f52f857c83eab2dc645d1c1dc2c916cb5174942
Michael Caldwell <michaelc@yingster.net>
2016-02-29 10:14:53 -0800

    Improving performance by using sieve and not storing results as strings.

--- problem_037.py
+++ problem_037.py
@@ -3,36 +3,36 @@
 print("Project Euler - Problem 37:")
 print("Find the sum of the only eleven primes that are both truncatable from left to right and right to left.\n")
 
-def isPrime (number):
-    import math
-    limit = int(math.sqrt(number))+1
-    for index in range(3, limit, 2):
-        if number % index == 0:
-            return False
-    return True
-
-primes = ["2"]
+limit = 1000000
+primes = [0, 0] + [1] * (limit-2)
 count = 11
 sum = 0
 
-for n in range(3, 1000000, 2):
-    if isPrime(n) == True:
-        nstr = str(n)
-        primes.append(nstr)
+for n in range(len(primes)):
+    # Find primes using sieve
+    if primes[n] == 1:
+        primes[n] = n
+        m = 2
+        while m * n < limit:
+            primes[m * n] = 0
+            m += 1
 
+        # Look for truncatability
         found = True
+        nstr = str(n)
         for i in range(1, len(nstr)):
-            if not nstr[i:] in primes:
+            if primes[int(nstr[i:])] == 0:
                 found = False
                 break
-            if not nstr[:i] in primes:
+            if primes[int(nstr[:i])] == 0:
                 found = False
                 break
+
+        # Record results
         if found == True and n > 10:
             sum += n
             count -= 1
+            if count == 0:
+                break
 
-    if count == 0:
-        break
-
-print sum
+print(sum)
