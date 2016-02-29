b1e6670233d814bc11f938f9d0ac8172e6b87d5d
Michael Caldwell <michaelc@yingster.net>
2016-02-28 21:04:13 -0800

    Adding solution for problem 38 in Python.

---
+++ problem_038.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+print("Project Euler - Problem 38:")
+print("What is the largest 1 to 9 pandigital that can be formed by multiplying a fixed number by 1, 2, 3, ...?\n")
+
+def isPandigital(n):
+    for i in range(1,10):
+        if str(i) not in n:
+            return False
+    return True
+
+max = 0
+for i in range(1, 1000):
+    number = ""
+    multiplier = 1
+    while len(number) < 9:
+        number += str(i * multiplier)
+        multiplier += 1
+    if len(number) == 9 and isPandigital(number) and int(number) > max:
+        max = int(number)
+
+print(max)
