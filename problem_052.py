f48c75b6d4df8ce3fefd342970472013ac12726f
Michael Caldwell <michaelc@yingster.net>
2016-03-21 07:51:36 -0700

    Adding solution for problem 52 in Python.

---
+++ problem_052.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+print("Project Euler - Problem 52:")
+print("Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits.\n")
+
+answer = 0
+
+for x in range(1000000):
+    sig = sorted(str(x))
+    for n in range(2, 7):
+        if sig == sorted(str(x * n)):
+            answer = x
+        else:
+            break
+
+    if answer != 0:
+        break
+
+print(answer)
