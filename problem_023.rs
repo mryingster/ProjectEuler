43710b7e2fa906ffffdb415c37905bd4245149a9
Michael Caldwell <michaelc@yingster.net>
2016-04-13 09:30:36 -0700

    Solving in a new way to reduce computation time.

--- problem_023.rs
+++ problem_023.rs
@@ -1,33 +1,33 @@
 // -*- compile-command: "rustc -o problem_023_rs problem_023.rs" -*-
 // Copyright (c) 2016 Michael Caldwell
 
-
-fn divisor_sum(n: usize) -> usize {
-    let mut s = 0;
-    for d in 1..(n/2+1) {
-        if n % d == 0 {
-            s += d;
-        }
-    }
-    return s;
-}
-
 fn main() {
     println!("Project Euler - Problem 23:");
     println!("Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.\n");
 
-    const LIMIT: usize = 28123;
-    let mut abundant = Vec::new();
+    const LIMIT: usize = 28123; // Given as largest abundant sum
 
-    for n in 12..LIMIT {
-        if divisor_sum(n) > n {
+    // Find sums of all numbers less than LIMIT
+    // and Isolate abundant numbers into vector
+    let mut sums: [usize; LIMIT] = [0; LIMIT];
+    let mut abundant = Vec::new();
+    for n in 1..LIMIT {
+        for m in 2..LIMIT {
+            let t = n * m;
+            if t > LIMIT {
+                break;
+            }
+            sums[t] += n;
+        }
+        if sums[n] > n{
             abundant.push(n);
         }
     }
 
+    // Add each pair of abundant numbers, track in table of bools
     let mut abundant_sum: [bool; LIMIT] = [false; LIMIT];
     for a in 0..abundant.len() {
-        for b in 0..abundant.len() {
+        for b in a..abundant.len() {
             let n = abundant[a] + abundant[b];
             if n >= LIMIT {
                 break;
@@ -36,6 +36,7 @@ fn main() {
         }
     }
 
+    // Sum all the numbers that are not a sum of 2 abundant numbers (false)
     let mut sum = 0;
     for n in 0..(LIMIT-1) {
         if abundant_sum[n] == false {
