4852a4551d4c5803e985edb45ec6a2db6dd5891a
Michael Caldwell <michaelc@yingster.net>
2016-04-26 10:29:20 -0700

    Speeding up solution in Python using a sieve.

--- problem_035.py
+++ problem_035.py
@@ -1,32 +1,39 @@
 #!/usr/bin/env python
-import math
+
+def rotate(n):
+    t = int(str(n)[-1]+str(n)[:-1])
+    return t
 
 print("Project Euler - Problem 35:")
 print("How many circular primes are there below one million?\n")
 
-def isPrime (n):
-    if n < 2: return False
-    if n == 2: return True
-    if n % 2 == 0: return False
-    limit = int(math.sqrt(n))
-    for index in range(3, limit+1, 2):
-        if n % index == 0:
-            return False
-    return True
-
-def isCircular(i):
-    n = str(i)
-    for j in range(len(n)-1):
-        n = n[-1]+n[:-1]
-        if isPrime(int(n)) == False:
-            return False
-    return True
-
 count = 0
 limit = 1000000
-for i in range(limit):
-    if isPrime(i) == True:
-        if isCircular(i) == True:
-            count += 1
+primes = [0, 0] + [1] * (limit-2)
+
+for n in range(len(primes)):
+    # Find primes using sieve
+    if primes[n] == 1:
+        primes[n] = n
+        m = 2
+        while m * n < limit:
+            primes[m * n] = 0
+            m += 1
+
+for i in range(len(primes)):
+    if primes[i] == 0:
+        continue
+
+    candidate = True
+
+    n = rotate(i)
+    while n != i:
+        if primes[n] == 0:
+            candidate = False
+            break
+        n = rotate(n)
+
+    if candidate == True:
+        count += 1
 
 print(count)
