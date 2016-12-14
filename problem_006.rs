1e3b580099b2dd5d841aea61898a7e05122b2612
Michael Caldwell <michaelc@yingster.net>
2016-12-14 09:57:48 -0800

    Renaming variables to make code more clear.

--- problem_006.rs
+++ problem_006.rs
@@ -5,15 +5,15 @@ fn main() {
     println!("Project Euler - Problem 6");
     println!("Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.\n");
 
-    let mut sum = 0;
-    let mut squ = 0;
+    let mut sum_of_squares: i32 = 0;
+    let mut square_of_sums: i32 = 0;
 
     for n in 1..101 {
-        sum += n*n;
-        squ += n;
+        sum_of_squares += n*n;
+        square_of_sums += n;
     }
 
-    squ *= squ;
+    square_of_sums *= square_of_sums;
 
-    println!("{}", squ-sum);
+    println!("{}", square_of_sums - sum_of_squares);
 }
