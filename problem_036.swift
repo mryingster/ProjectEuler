c52a87c04fe1b972ca717d78ae18a978822ada68
Michael Caldwell <michaelc@yingster.net>
2017-03-31 13:52:19 -0700

    Fixing problem 36 so it is compatible with Swift 3.0 syntax.

--- problem_036.swift
+++ problem_036.swift
@@ -5,15 +5,18 @@
 print("Project Euler - Problem 36:")
 print("Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.\n")
 
-func reverseString(let s:String) -> String {
+func reverseString(s:String) -> String {
     var r = ""
-    for var i=s.characters.count-1; i>=0; i-- {
-        r.append(s[s.startIndex.advancedBy(i)])
+
+    for i in (0 ..< s.characters.count).reversed() {
+        r.append(s.characters[s.index(s.startIndex, offsetBy:i)])
     }
+
     return r
 }
 
-func reverseInt(var n:Int) -> Int {
+func reverseInt(n:Int) -> Int {
+    var n = n
     var r = 0
     while n > 0 {
         r *= 10
@@ -28,10 +31,10 @@ let limit = 1000000
 let debug = false
 
 for n in 0...limit {
-    let nr = reverseInt(n)
+    let nr = reverseInt(n: n)
     if nr == n {
         let b = String(n, radix: 2)
-        let br = reverseString(b)
+        let br = reverseString(s: b)
         if b == br {
             if debug == true {
                 print(n, nr, b, br)
