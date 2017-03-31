827c0e0f907a9a86e1d7a745db4ba5470aeb50bd
Michael Caldwell <michaelc@yingster.net>
2017-03-31 14:00:51 -0700

    Updating problem 39 to support Swift 3.0 syntax. Adding comments, and better formatting.

--- problem_039.swift
+++ problem_039.swift
@@ -6,20 +6,34 @@ import Cocoa
 print("Project Euler - Problem 39:")
 print("Find the perimeter of a right triangle (p) that has the most possible solutions, where the perimiter is less than or equal to 1000.\n")
 
-var perimeter = [Int](count: 1001, repeatedValue: 0)
-var max = 0
+let limit = 1000
+var results = [Int](repeating: 0, count: limit + 1)
 
-for a in 1...1000 {
-    for b in a...1000 {
-        let cFloat = sqrt(Float((a*a) + (b*b)))
-        let cInt = Int(cFloat)
-        if cFloat != Float(cInt) { continue }
-        let sum = a + b + cInt
-        if sum > 1000 { continue }
-        perimeter[sum] += 1
-        if perimeter[sum] > perimeter[max] {
-            max = sum
+for a in 1 ... limit {
+    for b in a ... limit {
+        let c_float :Double = sqrt(Double((a * a) + (b * b)))
+        let c = Int(c_float)
+
+        // Make sure result is integer
+        if Double(c) != c_float {
+            continue
         }
+
+        // Make sure not more than limit
+        let p = a + b + c
+        if p > limit {
+            continue
+        }
+
+        // Track results
+        results[p] += 1
+    }
+}
+
+var max = 0
+for i in 0 ..< results.count {
+    if results[i] > results[max] {
+        max = i
     }
 }
 
