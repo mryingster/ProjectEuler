9dbdd2e418dae059a766c6eab9373e30fd3f9477
Michael Caldwell <michaelc@yingster.net>
2017-05-09 16:08:47 -0700

    Improving solution time for problem 4 in Bash by pre-screening solutions before using computationg time to reverse the number.

--- problem_004.sh
+++ problem_004.sh
@@ -4,14 +4,23 @@ echo "Project Euler - Problem 4"
 echo "Find the largest palindrome made from the product of two 3-digit numbers."
 echo
 
-answer=12321
+answer=0
 
 for (( a=999; a>99; a-- )); do
+    ((c=a*a))
     for (( b=a; b<1000; b++ )); do
-        c=$(($a*$b))
-        if [ $c -gt $answer ]; then
-            if [ $c -eq $(echo -n "$c" | rev) ]; then
-                answer=$c
+        ((c=c+a))
+
+        # Pre-screen by making sure it's bigger
+        if [[ $c -gt $answer ]]; then
+
+            # Pre screen again by making sure it could be palindrome
+            if [[ ${c:5:1} -eq ${c:0:1} ]]; then
+
+                # Check if palindrome
+                if [[ $c -eq $(echo -n "$c" | rev) ]]; then
+                    answer=$c
+                fi
             fi
         fi
     done
