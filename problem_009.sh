16a96e5fc2cdfb83e4c277565efdabb813f433d7
Michael Caldwell <michaelc@yingster.net>
2017-05-11 06:36:48 -0700

    Speeding up solution for problem 9 in Bash by simplifying computation and check.

--- problem_009.sh
+++ problem_009.sh
@@ -4,17 +4,17 @@ echo "Project Euler - Problem 9"
 echo "There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc."
 echo
 
-for (( a=1; a<1000; a++ )); do
-    aa=$(($a*$a))
-    for (( b=$a+1; b<1000; b++ )); do
-        leftside=$(($aa+($b*$b)))
-        for (( c=$b+1; c<1000; c++ )); do
-            # Check for appropriate c
-            rightside=$(($c*$c))
-            [[ $leftside -lt $rightside || $(($a+$b+$c)) -gt 1000 ]] && break
- 
-            # See if sum is 1000
-            [[ $leftside -eq $rightside && $(($a+$b+$c)) -eq 1000 ]] && echo $(($a*$b*$c)) && exit
-         done
+target=1000
+reducedTarget=$((target / 2))
+
+for (( a=1; a<reducedTarget; a++ )); do
+    aa=$((a * a))
+    for (( b=a; b<reducedTarget; b++ )); do
+        leftside=$((aa + (b * b)))
+        
+        c=$((target - a - b))
+        cc=$((c * c))
+
+        [[ $leftside -eq $cc ]] && echo $(($a*$b*$c)) && exit
     done
 done
