6016a532aa26bd2151d8f17524ddeb3f3cc9c0c5
Michael Caldwell <michaelc@yingster.net>
2017-11-30 19:50:00 -0800

    Fixing formatting on Python solution for problem 74

--- problem_074.py
+++ problem_074.py
@@ -3,46 +3,48 @@
 print("Project Euler: 74")
 print("How many factorial chains, with a starting number below one million, contain exactly sixty non-repeating terms?\n")
 
-Factorials={}
+Factorials = {}
 
 def Factorial(Input):
-    Output=1
+    Output = 1
     if Input in Factorials:
         return Factorials[Input]
-    for i in range(1,Input+1):
-        Output=Output*i
+    for i in range(1, Input + 1):
+        Output = Output*i
     Factorials.update({Input:Output})
     return Output
 
 def Digits(Input):
-    Temp=Input
-    Output=0
-    while Temp>0:
-        Output+=Factorial(Temp%10)
-        Temp=Temp/10
+    Temp = Input
+    Output = 0
+    while Temp > 0:
+        Output += Factorial(Temp % 10)
+        Temp = Temp / 10
     return Output
 
 #Main Variables
-Count=0
-History={}
+Count = 0
+History = {}
 
-for i in range(1,1000000):
+for i in range(1, 1000000):
     #print i,
-    NumbersEncountered=[i]
-    LoopCount=1
-    CurrentNum=Digits(i)
+    NumbersEncountered = [i]
+    LoopCount = 1
+    CurrentNum = Digits(i)
     while not CurrentNum in NumbersEncountered:
         #print CurrentNum,
-        LoopCount+=1
+        LoopCount += 1
         NumbersEncountered.append(CurrentNum)
-        CurrentNum=Digits(CurrentNum)
+        CurrentNum = Digits(CurrentNum)
         if CurrentNum in History:
-            LoopCount+=History[CurrentNum]
+            LoopCount += History[CurrentNum]
             break
-    #print "Count: "+str(LoopCount)
-    if LoopCount==60:
+
+    #print("Count: %d" %str(LoopCount))
+    if LoopCount == 60:
         #print i
-        Count+=1
+        Count += 1
     History.update({i:LoopCount})
 
 print("Chains with 60 terms: %d" % Count)
+
