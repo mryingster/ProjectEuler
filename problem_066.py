f0e681c2d5b978487dd25d1168ac1815b651328a
Michael Caldwell <michaelc@yingster.net>
2020-01-07 18:33:00 -0800

    Improving performace of Python solution for problem 66

--- problem_066.py
+++ problem_066.py
@@ -4,47 +4,51 @@ import math
 print("Project Euler - Problem 66")
 print("Using the equation, x^2 - Dy^2 = 1, find the value of D <= 1000 in minimal solutions of x for which the largest value of x is obtained.\n")
 
-def diophantine(a, debug):
+def diophantine(Number, Sqrt_N, debug):
+    a = Sqrt_N
     d = 1
     m = 0
 
-    num1 = 1
+    # First Fraction = 1/n^(1/2)
     num0 = a
-    den1 = 0
     den0 = 1
 
+    # Second Fraction = 1/0
+    num1 = 1
+    den1 = 0
+
     # Continued fraction until equation equals 1
-    while ((num0 ** 2) - (number * den0 ** 2)) != 1:
+    while ((num0 ** 2) - (Number * den0 ** 2)) != 1:
         m = d * a - m
-        d = (number - m * m) / d
-        a = (a0 + m) / d
+        d = (Number - m ** 2) / d
+        a = (Sqrt_N + m) / d
 
         num2 = num1
-        num1 = num0
         den2 = den1
+
+        num1 = num0
         den1 = den0
 
         num0 = a * num1 + num2
         den0 = a * den1 + den2
 
     if debug == True:
-        print("%s^2 - %s x %s^2" % (num0 , number, den0)) #DEBUG
+        print("%s^2 - %s x %s^2" % (num0 , Number, den0)) #DEBUG
 
     return num0
 
-results = [0, 0, 0]
+result = 0
 debug = False
 
-for number in range(3, 1001):
+for n in range(3, 1001):
     # Skip square numbers
-    a0 = int(math.sqrt(number))
-    if a0 ** 2 != number:
-        result = diophantine(a0, debug)
-    else:
-        result = 0
+    a = int(math.sqrt(n))
+    if a ** 2 == n: continue
+    r = diophantine(n, a, debug)
 
     # Record number
-    results.append(result)
+    if r > result:
+        result = r
 
 # Print maximum stored result
-print("%s (%s)" % (results.index(max(results)), max(results)))
+print("%s" % result)
