b9b59955e233b1cb3b178a054f27d7c5b2bc80e3
Michael Caldwell <michael@coastalvectors.com>
2021-01-12 06:50:18 -0800

    Refactoring solution to 117 to make it cleaner

--- problem_117.py
+++ problem_117.py
@@ -1,44 +1,49 @@
 #!/usr/bin/env python
 
-print("Project Euler - Problem 117:")
-print("Using a combination of grey square tiles and oblong tiles chosen from: red tiles (measuring two units), green tiles (measuring three units), and blue tiles (measuring four units), how many ways can a row measuring fifty units in length be tiled?\n")
-
-def recurse(a):
+def recurse(a, limit):
     if sum(a) == limit:
-        combinations.append(a)
-        return
+        return[a]
+
     if sum(a) > limit:
-        return
+        return []
 
+    combinations = []
     if len(a) == 0 or a[-1] <= 1:
-        recurse(a + [1])
+        combinations += recurse(a + [1], limit)
     if len(a) == 0 or a[-1] <= 2:
-        recurse(a + [2])
+        combinations += recurse(a + [2], limit)
     if len(a) == 0 or a[-1] <= 3:
-        recurse(a + [3])
+        combinations += recurse(a + [3], limit)
     if len(a) == 0 or a[-1] <= 4:
-        recurse(a + [4])
+        combinations += recurse(a + [4], limit)
+    return combinations
 
-def num_permutations(l):
+def num_permutations(l, factorials):
     numerator = factorials[len(l)]
     denominator = 1
     for i in set(l):
         denominator *= factorials[l.count(i)]
     return numerator / denominator
 
-# Preload factorials
-limit = 50
-factorials = [1]
-for i in range(1, limit + 1):
-    factorials.append(factorials[-1] * i)
+def __main__():
+    limit = 50
 
-# Find all combinations with recursion
-combinations = []
-recurse([])
+    print("Project Euler - Problem 117:")
+    print("Using a combination of grey square tiles and oblong tiles chosen from: red tiles (measuring two units), green tiles (measuring three units), and blue tiles (measuring four units), How many ways can a row measuring fifty units in length be tiled?\n")
 
-# Go through combinations and compute permutations
-count = 0
-for i in combinations:
-    count += num_permutations(i)
+    # Recurse to find tile combinations
+    combinations = recurse([], limit)
 
-print(count)
+    # Preload factorials
+    factorials = [1]
+    for i in range(1, limit + 1):
+        factorials.append(factorials[-1] * i)
+
+    # Go through combinations and permute them
+    count = 0
+    for i in combinations:
+        count += num_permutations(i, factorials)
+
+    print(count)
+
+__main__()
